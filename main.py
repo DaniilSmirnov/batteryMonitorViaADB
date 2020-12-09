2a67d8899abffc5e22f0eef2986ae3e234ecb498
di.smirnov <di.smirnov@corp.vk.com>
2020-12-09 20:28:15 +0300

    beatify

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ import datetime
 
 client = AdbClient(host="127.0.0.1", port=5037)
 
-#client.remote_connect("172.24.62.148", 5555)  # подсоединяем девайсы по adb via wifi
-#client.remote_connect("172.24.62.148", 5555)
+# client.remote_connect("172.24.62.148", 5555)  # подсоединяем девайсы по adb via wifi
+# client.remote_connect("172.24.62.148", 5555)
 
 devices = client.devices()
 
 i = 0
 iter_count = 10  # Количество промежутков для измерения
 
-for device in devices:
+while i != iter_count:
+    for device in devices:
+        print(str(datetime.datetime.now().hour) + ":" + str(datetime.datetime.now().minute))
+        print('device ' + str(device.get_serial_no()))
+        print('battery ' + str(device.get_battery_level()))
 
-    if i == iter_count:
-        break
-
-    print(str(datetime.datetime.now().hour) + ":" + str(datetime.datetime.now().minute))
-    print('device ' + str(device.get_serial_no()))
-    print('battery ' + str(device.get_battery_level()))
+        sleep(600)  # интервал в 10 минут
 
     i += 1
-    sleep(600)  # интервал в 10 минут
